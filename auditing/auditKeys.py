e65c7b47bce6af1b6bcb40953328c7a918ab61e7
Justin Martinez <jpmartee@gmail.com>
2016-10-14 18:22:15 -0700

    Script to count key types

---
+++ auditing/auditKeys.py
@@ -0,0 +1,26 @@
+import xml.etree.cElementTree as ET
+import pprint
+
+def audit_keys(osmfile):
+	"""
+	Function parses through xml file to count key types for nodes 
+	and ways
+
+	Args:
+		osmfile(xml): OpenStreetMaps data in xml format
+	Returns:
+		keys(set): A dictionary of keys/counts in the file
+	"""
+	osm_file = open(osmfile, "r")
+	keys = dict()
+	for event, elem in ET.iterparse(osm_file, events=("start",)):
+		if elem.tag == "node" or elem.tag == "way":
+			for tag in elem.iter("tag"):
+				key = tag.attrib['k']
+				if key in keys:
+					keys[key] += 1
+				else:
+					keys[key] = 1
+	return keys
+
+pprint.pprint(audit_keys('santa-cruz_california.osm')) 
